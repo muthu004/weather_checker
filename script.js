d5947f4708c68a5e3dee730f53ad3538f8c87160
Muthukumar V <www.goe2004@gmail.com>
2024-06-26 17:04:31 +0530

    weather checker

--- script.js
+++ script.js
@@ -50,19 +50,19 @@ async function fetchWeather() {
     function getWeatherPicture(weatherId) {
       switch (true) {
         case weatherId >= 200 && weatherId < 300:
-          return `thunder.png`;
+          return `./assets/thunder.png`;
         case weatherId >= 300 && weatherId < 400:
-          return `drizzle.png`;
+          return `./assets/drizzle.png`;
         case weatherId >= 500 && weatherId < 600:
-          return "rainy-day.png";
+          return "./assets/rainy-day.png";
         case weatherId >= 600 && weatherId < 700:
-          return `snow.png`;
+          return `./assets/snow.png`;
         case weatherId >= 700 && weatherId < 800:
-          return `fog.png`;
+          return `./assets/fog.png`;
         case weatherId === 800:
-          return `sun.png`;
+          return `./assets/sun.png`;
         case weatherId >= 801 && weatherId < 810:
-          return `cloudy.png`;
+          return `./assets/cloudy.png`;
 
         default:
           return "not defined";
